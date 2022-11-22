4b2e6c198bddd37d8b2a3e4b994ced5059cf8e29
billonium <billy.sethgrimmett@gmail.com>
2022-11-22 22:40:03 +0000

    Added footer

--- php/footer.php
+++ php/footer.php
@@ -1,3 +1,12 @@
 <div class="footer">
-    <p>Footer</p>
+    <p style="text-align: right; padding-right: 10px">Contact:<br>
+        Email: someone@example.com<br>
+        Phone: +44 1632 960550<br>
+        Fax: +44-208-1234567<br>
+        BBM Pin: 7AFC1E30<br>
+        Latitude: +34.5597880943659<br>
+        Longitude: +69.21103477189384<br>
+        Skype: love.xampp
+    </p>
+    <p>2022 ©</p>
 </div>
--- php/header.php
+++ php/header.php
@@ -1,5 +1,5 @@
 <ul>
-    <li><a href="../index.html" style="color: #f3f0d1">Home</a></li>
+    <li><a href="../index.html" style="color: #f3f0d1"><img src="../images/logo.png" alt="Home" style="width: 16px; height: 16px;"></a></li>
     <li><a href="../html/cv.html" style="color: #f3f0d1">CV</a></li>
     <li><a href="../html/login.html" style="color: #f3f0d1">Login</a></li>
     <li><a href="../html/weather-data.html" style="color: #f3f0d1">Weather Data</a></li>
