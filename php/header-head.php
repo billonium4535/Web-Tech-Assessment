e42c3ee26efd5c0bdb0cb18a9540224c0fb93280
billonium <billy.sethgrimmett@gmail.com>
2022-11-22 04:14:56 +0000

    Finished nav-bar

---
+++ php/header-head.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="../css/navbar.css">
+<link rel="stylesheet" href="../css/index_styles.css">
--- php/header.php
+++ php/header.php
@@ -1,2 +1,8 @@
-<?php
-echo "test";
+<ul>
+    <li><a class="active" href="../index.html">Home</a></li>
+    <li><a href="../html/cv.html">CV</a></li>
+    <li><a href="../html/login.html">Login</a></li>
+    <li><a href="../html/weather-data.html">Weather Data</a></li>
+</ul>
+<div id="currentTime" class="right">Time Loading...</div>
+<script src="../js/date-time.js"></script>
