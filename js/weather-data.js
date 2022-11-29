d1df21318fc0e49c2cfb6d2bbf67cb76fd2242a8
billonium <billy.sethgrimmett@gmail.com>
2022-11-29 23:29:11 +0000

    Added other graphs

--- js/weather-data.js
+++ js/weather-data.js
@@ -9,11 +9,11 @@ function displayGraph() {
     }
     */
 
-    var daily = JSON.parse(dailydata_london);
-    var forecast = JSON.parse(forecast_london);
+    var daily = JSON.parse(dailydata_stoke);
+    var forecast = JSON.parse(forecast_stoke);
 
     // Searches for the label "lon" and inserts the coordinates
-    document.getElementById('lon').innerHTML = daily.coord.lon;
+    // document.getElementById('lon').innerHTML = daily.coord.lon;
 
     // Reads in the dates from the js file and returns a list
     this.dateList = forecast.list.map(list => {
@@ -25,8 +25,18 @@ function displayGraph() {
         return list.main.temp;
     });
 
-    // Creates a 2d line graph using the ID "myChart"
-    var ctx = document.getElementById('myChart').getContext('2d');
+
+    this.windList = forecast.list.map(list => {
+        return list.wind.speed;
+    });
+
+
+    this.humidityList = forecast.list.map(list => {
+        return list.main.humidity;
+    });
+
+    // Creates a 2d line graph using the ID "temperatureChart"
+    var ctx = document.getElementById('temperatureChart').getContext('2d');
     this.chart = new Chart(ctx, {
         type: "line",
         data: {
@@ -35,12 +45,89 @@ function displayGraph() {
                 {
                     // Graph configuration
                     label: "Temperature",
-                    backgroundColor: "lightblue",
+                    backgroundColor: "yellow",
                     borderColor: "blue",
                     fill: false,
                     data: this.temperatureList
                 }
             ]
-        }
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    text: "Stoke-On-Trent - Temperature",
+                    font: {
+                        family: "Helvetica Neue",
+                        size: 16,
+                    }
+                }
+            }
+        },
+    })
+
+    // Creates a 2d line graph using the ID "windChart"
+    var ctx = document.getElementById('windChart').getContext('2d');
+    this.chart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: this.dateList,
+            datasets: [
+                {
+                    // Graph configuration
+                    label: "Wind",
+                    backgroundColor: "yellow",
+                    borderColor: "blue",
+                    fill: false,
+                    data: this.windList
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    text: "Stoke-On-Trent - Wind",
+                    font: {
+                        family: "Helvetica Neue",
+                        size: 16,
+                    }
+                }
+            }
+        },
+    })
+
+    // Creates a 2d line graph using the ID "humidityChart"
+    var ctx = document.getElementById('humidityChart').getContext('2d');
+    this.chart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: this.dateList,
+            datasets: [
+                {
+                    // Graph configuration
+                    label: "Humidity",
+                    backgroundColor: "yellow",
+                    borderColor: "blue",
+                    fill: false,
+                    data: this.humidityList
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    text: "Stoke-On-Trent - Humidity",
+                    font: {
+                        family: "Helvetica Neue",
+                        size: 16,
+                    }
+                }
+            }
+        },
     })
 }
