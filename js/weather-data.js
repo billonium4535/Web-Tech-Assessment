762be90a80103eb15a03305a71f943603fa25e7b
billonium <billy.sethgrimmett@gmail.com>
2022-11-29 18:18:27 +0000

    Added comments, fixed weather-data.js. Added json files.

--- js/weather-data.js
+++ js/weather-data.js
@@ -1,5 +1,5 @@
-function changeCity() {
-    var currentCity = document.getElementById("selectCity").value;
+function displayGraph() {
+    /*var currentCity = document.getElementById("selectCity").value;
     if (currentCity === "stoke") {
         var daily = JSON.parse(dailydata_stoke);
         var forecast = JSON.parse(forecast_stoke);
@@ -7,16 +7,25 @@ function changeCity() {
         var daily = JSON.parse(dailydata_london);
         var forecast = JSON.parse(forecast_london);
     }
+    */
+
+    var daily = JSON.parse(dailydata_london);
+    var forecast = JSON.parse(forecast_london);
+
+    // Searches for the label "lon" and inserts the coordinates
     document.getElementById('lon').innerHTML = daily.coord.lon;
 
+    // Reads in the dates from the js file and returns a list
     this.dateList = forecast.list.map(list => {
         return list.dt_txt;
     });
 
+    // Reads in the temperature from the js file and returns a list
     this.temperatureList = forecast.list.map(list => {
         return list.main.temp;
     });
 
+    // Creates a 2d line graph using the ID "myChart"
     var ctx = document.getElementById('myChart').getContext('2d');
     this.chart = new Chart(ctx, {
         type: "line",
@@ -24,6 +33,7 @@ function changeCity() {
             labels: this.dateList,
             datasets: [
                 {
+                    // Graph configuration
                     label: "Temperature",
                     backgroundColor: "lightblue",
                     borderColor: "blue",
