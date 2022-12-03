ab24ed4e3d2748305a8395278148cc3a7ab72f3b
billonium4535 <billy.sethgrimmett@gmail.com>
2022-12-03 00:09:01 +0000

    Started display.php

--- php/display.php
+++ php/display.php
@@ -1,1 +1,22 @@
 <?php
+
+
+$file = fopen("../csv/publications.csv", "r");
+
+if (!$file) {
+    return null;
+}
+
+//while (($data = fgetcsv($file)) !== FALSE) {
+//    if $data
+//    echo "<tr>";
+//    echo "<td>" . $data[0] . "</td>";
+//    echo "<td>" . $data[1] . "</td>";
+//    echo "<td>" . $data[2] . "</td>";
+//    echo "<td>" . $data[3] . "</td>";
+//    echo "<td>" . $data[4] . "</td>";
+//    echo "<td>" . $data[5] . "</td>";
+//    echo "</tr>";
+//}
+
+fclose($file);
